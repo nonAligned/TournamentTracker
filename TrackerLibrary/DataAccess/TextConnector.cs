041101e084dc1944ce009133a11b4f610f9449cd
Momcilo Savic <savic227@gmail.com>
2024-06-21 11:28:30 +0200

    Added SaveToPrizeFile method in TextConnectorProcessor class

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
         // TODO - Wire up the CreatePrize for text files.
+        private const string PrizesFile = "PrizeModels.csv";
+
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModel();
 
-            return model;
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            model.Id = currentId;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
         }
     }
 }
