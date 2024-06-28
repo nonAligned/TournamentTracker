197605abcbe92efddcce223062fc4df683debc80
Momcilo Savic <savic227@gmail.com>
2024-06-28 11:34:51 +0200

    Addded id field in MatchupModel

--- TrackerLibrary/Models/MatchupModel.cs
+++ TrackerLibrary/Models/MatchupModel.cs
@@ -9,6 +9,10 @@ namespace TrackerLibrary.Models
     public class MatchupModel
     {
         /// <summary>
+        /// The unique identifier for the matchup.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
         /// Represents a list of all entries in the current matchup.
         /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
